5f0e8c1db9c42a6aeab022b8bebc58fed95b7416
immanjohn991 <immanjohn991@gmail.com>
2022-08-24 04:14:05 -0700

    Addressed the code review comments

--- ReactSkia/views/common/RSkTextUtils.cpp
+++ ReactSkia/views/common/RSkTextUtils.cpp
@@ -83,7 +83,6 @@ void drawText(std::shared_ptr<Paragraph>& paragraph,
     Rect frame = layout.frame;
     int textFrameHeight=(std::isnan(props.textAttributes.lineHeight)) ? frame.size.height : props.textAttributes.lineHeight;
     SkAutoCanvasRestore save(canvas, true);
-    SkColor bgColor=0;
     
     if(textFrameHeight <= 0){
         RNS_LOG_DEBUG(" Text Drawing Ignored, Height[" << textFrameHeight<<"]");
@@ -98,7 +97,7 @@ void drawText(std::shared_ptr<Paragraph>& paragraph,
     canvas->clipPath(clipPath, SkClipOp::kIntersect);
 
     if (props.backgroundColor ){
-        bgColor = RSkColorFromSharedColor(props.backgroundColor, SK_ColorTRANSPARENT);
+        SkColor bgColor = RSkColorFromSharedColor(props.backgroundColor, SK_ColorTRANSPARENT);
         if (bgColor){
             canvas->drawColor(bgColor);
         }
