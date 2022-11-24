49c02bbfb4b727d275343a530cca01728929bc41
immanjohn991 <immanjohn991@gmail.com>
2022-11-24 06:39:53 -0800

    Addressed review comments and added activity indicator animation stroke drawing code in onPaine fuction and added one test application for activity indicator

--- ReactSkia/components/RSkComponentActivityIndicator.cpp
+++ ReactSkia/components/RSkComponentActivityIndicator.cpp
@@ -1,14 +1,20 @@
 /*
- * Copyright (C) 1994-2021 OpenTV, Inc. and Nagravision S.A.
+ * Copyright (C) 1994-2022 OpenTV, Inc. and Nagravision S.A.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
+#include "include/core/SkPaint.h"
+#include "include/core/SkPath.h"
 
 #include <ReactSkia/activityindicator/react/renderer/components/activityindicator/ActivityIndicatorProps.h>
 #include "ReactSkia/components/RSkComponentActivityIndicator.h"
+#include "ReactSkia/views/common/RSkConversion.h"
 
 #include "ReactSkia/utils/RnsLog.h"
+
+#define ACTIVITY_INDICATOR_STROKE_WIDTH_RATIO             12.5
+#define ACTIVITY_INDICATOR_BG_STROKE_TRANSPARENCY_RATIO   20
 
 namespace facebook {
 namespace react {
@@ -25,11 +31,45 @@ RnsShell::LayerInvalidateMask  RSkComponentActivityIndicator::updateComponentPro
 void RSkComponentActivityIndicator::OnPaint(SkCanvas *canvas) {
   auto component = getComponentData();
   auto const &activityIndicatorProps = *std::static_pointer_cast<ActivityIndicatorProps const>(component.props);
-
   auto borderMetrics=activityIndicatorProps.resolveBorderMetrics(component.layoutMetrics);
   Rect frame = component.layoutMetrics.frame;
+  SkPaint paint;
+  SkScalar strokeWidth;
 
-  /*Yet to implement the activity indicator drawings...*/
+  paint.setAntiAlias(true);
+  paint.setStyle(SkPaint::kStroke_Style);
+
+  strokeWidth = (frame.size.width * ACTIVITY_INDICATOR_STROKE_WIDTH_RATIO) / 100;
+  if(!strokeWidth){
+    return;
+  }
+  paint.setStrokeWidth(strokeWidth);
+
+  if (activityIndicatorProps.color ){
+      SkColor strokeColor = RSkColorFromSharedColor(activityIndicatorProps.color, SK_ColorTRANSPARENT);
+      SkColor alphaVal = SkColorGetA(strokeColor);
+
+      if (strokeColor && alphaVal){
+          SkColor bgStrokeAlphaValue;
+          SkPath bgStrokePath;
+          SkPath fgStrokePath;
+          SkRect rect = SkRect::MakeXYWH(frame.origin.x, frame.origin.y, frame.size.width, frame.size.height);
+          
+          rect.inset(strokeWidth/2, strokeWidth/2);
+          bgStrokeAlphaValue = (alphaVal * ACTIVITY_INDICATOR_BG_STROKE_TRANSPARENCY_RATIO) / 100;
+          
+          if(bgStrokeAlphaValue){
+              bgStrokePath.addArc(rect, 0, 360);
+              paint.setColor(strokeColor);
+              paint.setAlpha(bgStrokeAlphaValue);
+              canvas->drawPath(bgStrokePath, paint);
+          }
+
+          fgStrokePath.addArc(rect, 0, 80);
+          paint.setColor(strokeColor);
+          canvas->drawPath(fgStrokePath, paint);
+      }
+  }
 }
 
 } // namespace react
